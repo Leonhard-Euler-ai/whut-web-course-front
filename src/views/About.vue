e24e90ce6ae15f9f752ffaf734254e1dcaec5d40
Leonhard-Euler-ai <873406454@qq.com>
2021-03-20 15:02:28 +0800

    安装element_ui插件并引入sass依赖

--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="about">
     <h1>This is an about page</h1>
+    <el-button @click="click">ui</el-button>
   </div>
 </template>
+
+<script>
+export default {
+  methods:{
+    click(){
+      this.$message.success("Hello")
+    }
+  }
+
+}
+</script>
