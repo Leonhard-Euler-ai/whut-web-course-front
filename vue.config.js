ff97da51b04d0e61c96b71ef11cbeadcc2641dbf
Leonhard-Euler-ai <873406454@qq.com>
2021-03-20 14:48:48 +0800

    增加两个配置文件.editorconfig和vue.config.js

---
+++ vue.config.js
@@ -0,0 +1,12 @@
+const path = require('path')
+const resolve = dir => path.join(__dirname, dir)
+module.exports = {
+  chainWebpack: config => {
+    config.resolve.alias
+      .set('@', resolve('src'))
+      .set('assets', resolve('src/assets'))
+      .set('views', resolve('src/views'))
+      .set('components', resolve('src/components'))
+      .set('network', resolve('src/network'))
+  }
+}
